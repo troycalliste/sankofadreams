fffa2c73df37fca1227d19c4661477d886c0152e
troycalliste <troycallist@gmail.com>
2020-06-08 13:29:44 -0400

    mailer

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_08_163829) do
+ActiveRecord::Schema.define(version: 2020_06_08_164916) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,10 @@ ActiveRecord::Schema.define(version: 2020_06_08_163829) do
   create_table "inquiries", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
+    t.string "email"
+    t.string "subject"
+    t.text "message"
   end
 
 end
